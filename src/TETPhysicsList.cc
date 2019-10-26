d2d359e13760477cc5a64a1a8b0ffc8cece2ce13
haeginh <haeginh@gmail.com>
2019-10-27 01:44:26 +0900

    simple physics for gamma/e- only

--- src/TETPhysicsList.cc
+++ src/TETPhysicsList.cc
@@ -57,7 +57,7 @@ TETPhysicsList::TETPhysicsList()
   new G4UnitDefinition( "mm2/g",  "mm2/g", "Surface/Mass", mm2/g);
   new G4UnitDefinition( "um2/mg", "um2/mg","Surface/Mass", um*um/mg);
 
-  // Hadron Elastic scattering
+/*  // Hadron Elastic scattering
   RegisterPhysics( new HadronElasticPhysicsHP(verb) );
 
   // Hadron Inelastic Physics
@@ -67,7 +67,7 @@ TETPhysicsList::TETPhysicsList()
   RegisterPhysics( new G4IonPhysics(verb));
 
   // Gamma-Nuclear Physics
-  RegisterPhysics( new GammaPhysics("gamma"));
+  RegisterPhysics( new GammaPhysics("gamma"));*/
 
 
   // EM physics
