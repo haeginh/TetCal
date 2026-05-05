8483d9cf6bee736d6397ab40f164dfafba9b4dab
haeginh <haeginh@gmail.com>
2026-05-05 16:20:12 -0400

    removed build dir

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_TetCal_FILE /Users/hanh4/workspace/TetCal/build/TetCal)
-set(__QT_DEPLOY_TARGET_TetCal_TYPE EXECUTABLE)
--- build_ODD/CMakeFiles/ODDCal.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for ODDCal.
-# This may be replaced when dependencies are built.
